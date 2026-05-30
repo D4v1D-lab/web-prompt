5ce45aa884b74b8a24e2ee440c52f3135a2156e3
root <root@DESKTOP-5J1GNK2.localdomain>
2026-05-30 18:14:31 -0500

    Fix all bugs: CSS issues, broken links, add single.html, search filtering, newsletter handling, SEO meta tags

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,6 @@ $(document).ready(function () {
   $('.menu-toggle').on('click', function () {
     $('.nav').toggleClass('showing');
     $('.nav ul').toggleClass('showing');
-
   });
 
   $('.posts').slick({
@@ -26,15 +25,47 @@ $(document).ready(function () {
         breakpoint: 825,
         settings: {
           slidesToShow: 1,
-          slidesToScroll: 1, 
+          slidesToScroll: 1,
           accessibility: false
         }
-      },
-
-      // You can unslick at a given breakpoint now by adding:
-      // settings: "unslick"
-      // instead of a settings object
+      }
     ]
   });
-});
 
+  // Search form filtering
+  $('input[name="search-term"]').on('input', function () {
+    var query = $(this).val().toLowerCase();
+
+    // Filter recent posts
+    $('.recent-post').each(function () {
+      var text = $(this).text().toLowerCase();
+      if (query === '' || text.indexOf(query) !== -1) {
+        $(this).show();
+      } else {
+        $(this).hide();
+      }
+    });
+
+    // Filter carousel slider posts
+    var found = false;
+    $('.posts .single-post').each(function () {
+      var text = $(this).text().toLowerCase();
+      if (query === '' || text.indexOf(query) !== -1) {
+        $(this).show();
+        found = true;
+      } else {
+        $(this).hide();
+      }
+    });
+  });
+
+  // Newsletter form handling
+  $('.section.contact form').on('submit', function (e) {
+    e.preventDefault();
+    var email = $(this).find('input[name="subs-term"]').val();
+    if (email) {
+      $(this).find('.btn').text('Suscrito!').css({ background: '#28a745', color: '#fff' });
+      $(this).find('input[name="subs-term"]').prop('disabled', true);
+    }
+  });
+});
